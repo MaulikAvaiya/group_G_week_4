debf021f58013d80ebdeff7a1a22884faf1eaba0
unknown <cherishpatel2001@gmail.com>
2024-09-27 18:33:57 -0400

    reverse string by himani

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to Group A's Week 4 Project!")
+	fmt.Println("Welcome to Group A's Week 4 Project!")
+	name := "Himani"
+	fmt.Println("Reversed String is: ", RevString(name))
 }
